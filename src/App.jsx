61e790ed5b758aceeb2a1fedc009d802682675a2
hasankarim18 <hallysust@gmail.com>
2023-04-11 00:33:19 +0600

    session storage with modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { Outlet, useLoaderData } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import React, { createContext, useEffect, useState } from "react";
+import React, { createContext,  useState } from "react";
+import Modal from "./components/utils/Modal";
 
 
 
@@ -10,13 +11,14 @@ export const CartContext = createContext([])
 
 
 const App = () => {
+  const [isOpen, setIsOpen] = useState(false)
   const { cartArray, products } = useLoaderData();
   const [cart, setCart] = useState(cartArray);
 
   const cartAlert = sessionStorage.getItem('alert') 
 
   if(cart.length >0 && cartAlert !== 'true'){
-    alert('You have items in cart')
+    setIsOpen(true)
     sessionStorage.setItem('alert', true)
   }
   
@@ -29,6 +31,7 @@ const App = () => {
             <Outlet />
           </div>
           <Footer />
+          <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
         </CartContext.Provider>
       </ProductContext.Provider>
     </>
